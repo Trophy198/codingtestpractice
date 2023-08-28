30e0dc217f4e7a2834257fc9d26bd5ba6175f5f3
Trophy198 <spsp8426@naver.com>
2023-08-28 13:52:32 +0900

    2차원배열 나선형으로 순회하면서 숫자증가

--- 0단계/soultion22.js
+++ 0단계/soultion22.js
@@ -1,8 +1,29 @@
-function solution(age) {
+function solution(n) {
+   
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
     
-    let year = 2022;
-    return year - age + 1;
+    let x = 0, y = 0;
+    let num = 1;
+    let dx = 0, dy = 1;  
+    
+    for (let i = 0; i < n * n; i++) {
+        matrix[x][y] = num;
+        num++;       
+        if (
+            x + dx < 0 || x + dx >= n ||
+            y + dy < 0 || y + dy >= n ||
+            matrix[x + dx][y + dy] !== 0
+        ) {
+            [dx, dy] = [dy, -dx];
+        }
+        x += dx;
+        y += dy;
+    }
+    
+    return matrix;
 }
 
 
-console.log(solution(40))
+console.log(solution(4))
+
+

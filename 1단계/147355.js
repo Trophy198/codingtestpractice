03c2c4595e8f24695e8a9e94b957f69a0fbf9c7d
Trophy198 <spsp8426@naver.com>
2023-10-18 20:45:25 +0900

    [Lv.1] 크기가 작은 부분 문자열(solution1 함수 추가)

--- 1단계/147355.js
+++ 1단계/147355.js
@@ -9,4 +9,19 @@ function solution(t,p) {
 return count
 }
 
-console.log(solution("3141592","271"))
+console.log(solution("3141592","271"));
+
+
+function solution1(t,p) {
+    let tLength = t.length;
+    let pLength = p.length;
+    let count = 0;
+    for(let i = 0; i <= tLength - pLength; i++) {
+      if(t.slice(i,i+pLength) <= p) {
+        count++;
+      }
+    }
+return count;
+}
+
+console.log(solution1("3141592","271"));

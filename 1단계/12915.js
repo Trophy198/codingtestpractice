23226e06d45d4bc3bdbba6a4e1f82881387d6645
Trophy198 <spsp8426@naver.com>
2023-12-07 16:38:07 +0900

    [Lv.1] 문자열 내 마음대로 정렬하기

---
+++ 1단계/12915.js
@@ -0,0 +1,13 @@
+function solution(strings, n) {
+    strings.sort();
+
+    strings.sort((a,b) => {
+        if(a[n] === b[n]) {
+            return 0;
+        }
+        return a[n] > b[n] ? 1 : -1;
+    });
+    return strings;
+}
+
+console.log(solution(["sun", "bed", "car"],1))

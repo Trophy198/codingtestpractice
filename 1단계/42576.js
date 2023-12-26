4b693dbeb05aa999e7862645e596f268342a2c5b
Trophy198 <spsp8426@naver.com>
2023-12-26 16:20:52 +0900

    [Lv.1] 완주하지 못한 선수

---
+++ 1단계/42576.js
@@ -0,0 +1,14 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+
+    for (let i = 0; i < completion.length; i++) {
+        if (participant[i] !== completion[i]) {
+            return participant[i];
+        }
+    }
+
+    return participant[participant.length - 1];
+}
+
+console.log(solution(["leo", "kiki", "eden"],["eden", "kiki"]))

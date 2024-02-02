cc6b82588a653f762b410a1baed52e8d274ff7c5
Trophy198 <spsp8426@naver.com>
2024-02-02 13:52:59 +0900

    [Lv.2] 예상 대진표

---
+++ 2단계/12985.js
@@ -0,0 +1,14 @@
+function solution(n, a, b) {
+    let round = 0; 
+
+    while (a - b !== 0) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        
+        round += 1;
+    }
+
+    return round;
+}
+
+console.log(solution(8, 4, 7));
